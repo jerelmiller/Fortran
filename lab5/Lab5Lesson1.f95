691b554fd17b078d0f5902ab527361cd076046b1
Jerel Miller <jermille@mines.edu>
2012-02-15 22:06:48 -0700

    Lesson 1 done

--- lab5/Lab5Lesson1.f95
+++ lab5/Lab5Lesson1.f95
@@ -23,12 +23,37 @@ INTEGER:: ans, guess
 
 ! Code the Problem Here
 
-CALL_RANDOM_SEED()
-x = CALL_RANDOM_NUMBER(x)
+! Generate a random number
+CALL random_seed()         
+CALL RANDOM_NUMBER(x)
 
-ans = 1+INT(x*10)
+ans = 1 + INT(x * 10)
 
-WRITE(*,*) ans
+! Generate a second random number so that ans is between 1 and 20
+CALL random_seed()         
+CALL RANDOM_NUMBER(x)
+
+ans = ans + 1 + INT(x * 10)
+
+! Prompt the user for input
+WRITE(*,*) "Welcome to the super duper number guessing game!"
+WRITE(*,*) "The computer has randomly chosen a number between 1 and 20. Please make your guess as to what it is: "
+
+!Loop until the number is guessed
+DO
+  
+  READ(*,*) guess
+  IF (guess > ans) THEN
+    WRITE(*,*) "Too high! Please guess again"
+  ELSE IF (guess < ans) THEN
+    WRITE(*,*) "Too low! Please guess again" 
+  END IF
+
+  IF (guess == ans) EXIT
+
+END DO
+
+WRITE(*,*) "You guessed it! It was", ans
 
 
 ! Say Goodbye
